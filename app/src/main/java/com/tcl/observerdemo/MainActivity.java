c7e314429cdff799952787c8a1f627f304705b70
GerogeYang <jian55661818@163.com>
2018-05-07 20:24:24 +0800

    third commit

--- app/src/main/java/com/tcl/observerdemo/MainActivity.java
+++ app/src/main/java/com/tcl/observerdemo/MainActivity.java
@@ -7,11 +7,10 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,Observer{
     private static final String TAG = MainActivity.class.getName();
     private Button btnRegister, btnUnRegister, btnSendMessage;
     private TextView textView;
-    private ButtonObserver observer;
     private ButtonListener listener;
     private String message;
     @Override
@@ -25,7 +24,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnSendMessage = findViewById(R.id.sendmessage);
         btnSendMessage.setOnClickListener(this);
         textView = findViewById(R.id.textview);
-        observer = new ButtonObserver();
         listener = new ButtonListener();
     }
 
@@ -33,24 +31,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.register:
-                listener.registerObserver(observer);
+                listener.registerObserver(this);
                 Toast.makeText(MainActivity.this,"观察者注册",Toast.LENGTH_SHORT).show();
                 break;
             case R.id.unregister:
-                listener.removeObserver(observer);
+                textView.setText("");
+                listener.removeObserver(this);
                 Toast.makeText(MainActivity.this,"取消注册",Toast.LENGTH_SHORT).show();
                 break;
             case R.id.sendmessage:
                 message = "My name is Mr Yang";
                 listener.sendMessage(message);
                 Toast.makeText(MainActivity.this,"发送消息："+message,Toast.LENGTH_SHORT).show();
-                String msg = observer.getMessage();
-                if (null != msg){
-                    textView.setText(msg);
-                } else {
-                    textView.setText("");
-                }
                 break;
         }
+    }
+
+    @Override
+    public void update(String message) {
+        textView.setText(message);
     }
 }
